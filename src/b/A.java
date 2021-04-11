c327d69f50da8835ee83a3bf24a7a7eac2ecdd48
Your Name <khaledbdt@gmail.com>
2021-04-10 23:41:12 -0400

    add

--- src/b/A.java
+++ src/b/A.java
@@ -1,6 +1,6 @@
-package jenkins.practice;
+package b;
 
-public class Jenkins {
+public class A {
 
 	public static void main(String[] args) {
 		test1();
@@ -13,6 +13,9 @@ public static void test1() {
 }
 public static void test2() {
 	System.out.println("print abc2");
+}
+public static void test3() {
+	System.out.println("print abc3");
 }
 	
 }
