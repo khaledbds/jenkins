72fcb572148dde6001e47bffb167215b4ceed815
Your Name <khaledbdt@gmail.com>
2021-04-10 22:58:16 -0400

    jenkins Practice

---
+++ src/jenkins/practice/Jenkins.java
@@ -0,0 +1,18 @@
+package jenkins.practice;
+
+public class Jenkins {
+
+	public static void main(String[] args) {
+		test1();
+		test2();
+		
+
+	}
+public static void test1() {
+	System.out.println("print abc1");
+}
+public static void test2() {
+	System.out.println("print abc2");
+}
+	
+}
